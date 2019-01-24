51c652a4f7853e888769e66ac0cc691df3ec47ee
advence-liz <1012515183@qq.com>
2019-01-24 19:47:17 +0800

    fix: index rewrite

--- example/index.js
+++ example/index.js
@@ -2,22 +2,24 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults() // { static: 'public' }
 const JsonServerRouter = require('../index')
+
 /**
  * @prop {string} root mock文件根目录默认为 'mock'
  * @prop {number} port 端口号跟json-server 一致
  * @prop {string} publicPath 生成默认首页的地址，跟json-server 一致默认为 'public'
  */
-
+const port = 3000
 const router = new JsonServerRouter({
   root: 'mock',
-  port: 3000,
+  port,
   publicPath: 'public'
 })
 
 server.use(middlewares)
 
 server.use(router.routes())
+server.use(router.rewrite())
 
-server.listen(3000, () => {
-  console.info('JSON Server is running')
+server.listen(port, () => {
+  console.info(`JSON Server is running on ${port}`)
 })
